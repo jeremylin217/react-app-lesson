9d85f7c1dd341feb9a3de1c6d9a9168e1fff960e
JeremyLin <didigh@gmail.com>
2019-04-09 08:56:10 +0800

    day2

--- src/epics/users.js
+++ src/epics/users.js
@@ -5,12 +5,15 @@ import { map, switchMap, catchError } from 'rxjs/operators'
 import { GET_USERS_REQUEST, GET_USER_REQUEST } from '@/constants/actionTypes'
 import { getUsersReceive, getUsersFailure } from '@/actions/users'
 import { getUserReceive, getUserFailure } from '@/actions/user'
+const { REACT_APP_API_ROOT } = process.env
+
+console.log('process.env: ', process.env)
 
 export const getUsers = action$ =>
   action$.pipe(
     ofType(GET_USERS_REQUEST),
     switchMap(action =>
-      ajax.getJSON(`https://jsonplaceholder.typicode.com/users`).pipe(
+      ajax.getJSON(`${REACT_APP_API_ROOT}/users`).pipe(
         map(users => getUsersReceive({ users })),
         catchError(error => of(getUsersFailure(error)))
       )
@@ -22,7 +25,7 @@ export const getUser = action$ =>
     ofType(GET_USER_REQUEST),
     map(action => action.payload.id),
     switchMap(id =>
-      ajax.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`).pipe(
+      ajax.getJSON(`${REACT_APP_API_ROOT}/users/${id}`).pipe(
         map(user => getUserReceive({ user })),
         catchError(error => of(getUserFailure(error)))
       )
